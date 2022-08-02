1e70f836104e1220dfea83df673d8fc0c0295b08
Anastasiya Evtukhova <stasja_195@mail.ru>
2022-08-02 21:50:54 +0300

    new

--- httpHW/src/main/java/Cat.java
+++ httpHW/src/main/java/Cat.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Cat {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cat {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private Integer upvotes;
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") Integer upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id +
+                "\n text: " + text +
+                "\n user: " + user +
+                "\n upvotes: " + upvotes;
+    }
+
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(Integer upvotes) {
+        this.upvotes = upvotes;
+    }
 }
--- httpHW/src/main/java/Main.java
+++ httpHW/src/main/java/Main.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Main {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static List<Cat> filterCats(List<Cat> cats, Integer upvotes) {
+
+        Stream<Cat> cat = cats.stream()
+                .filter(upvotesFiltred -> upvotesFiltred.getUpvotes() != upvotes && upvotesFiltred.getUpvotes() > 0);
+
+        return cat.collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5_000)
+                        .setSocketTimeout(30_000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request =
+                new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+
+        List<Cat> cats = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Cat>>() {
+        });
+
+        cats.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Отфильтрованный список");
+        System.out.println();
+
+        cats = filterCats(cats, null);
+
+        cats.forEach(System.out::println);
+
+
+    }
 }
